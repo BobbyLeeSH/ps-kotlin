7b162c826394abdffe9fe35a6bc22cd5ab81c593
Bobby-L <bobby@lendit.co.kr>
2020-06-25 19:42:15 +0900

    DP

---
+++ src/programmers/dp/ExpressWithN.kt
@@ -0,0 +1,42 @@
+package programmers.dp
+
+class ExpressWithN {
+    // https://programmers.co.kr/learn/courses/30/lessons/42895
+
+    fun solution(N: Int, number: Int): Int {
+        if (number == N) return 1
+        val nToValue = mutableMapOf(1 to mutableSetOf(N), 2 to mutableSetOf(), 3 to mutableSetOf(), 4 to mutableSetOf(), 5 to mutableSetOf(), 6 to mutableSetOf(), 7 to mutableSetOf(), 8 to mutableSetOf())
+        for (i in 2..8) {
+            for (j in 1 until i) {
+                nToValue[j]?.forEach { first ->
+                    nToValue[i - j]?.forEach { second ->
+                        nToValue[i]?.add(first + second)
+                        nToValue[i]?.add(first * second)
+
+                        nToValue[i]?.add(first - second)
+                        nToValue[i]?.add(second - first)
+
+                        if (second != 0) nToValue[i]?.add(first / second)
+                        if (first != 0) nToValue[i]?.add(second / first)
+                    }
+                }
+            }
+            val str = StringBuilder()
+            for (k in 1..i) {
+                str.append(N)
+            }
+            nToValue[i]?.add(str.toString().toInt())
+            if (nToValue[i]?.contains(number) == true) {
+                return i
+            }
+        }
+
+        return -1
+    }
+}
+
+/**
+ * 5
+ * 55 5+5 5-5 5*5 5/5 -> 55 10 0 25 1
+ * 555 55+5
+ */
---
+++ src/programmers/dp/IntegerTriangle.java
@@ -0,0 +1,35 @@
+package programmers.dp;
+
+public class IntegerTriangle {
+    // https://programmers.co.kr/learn/courses/30/lessons/43105
+    public int solution(int[][] triangle) {
+        if (triangle.length == 1) return triangle[0][0];
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                int first = Math.max(0, j - 1);
+                int second = Math.min(triangle[i-1].length - 1, j);
+                triangle[i][j] = Math.max(triangle[i-1][first], triangle[i-1][second]) + triangle[i][j];
+                System.out.print(triangle[i][j] + " ");
+            }
+            System.out.print(" ");
+        }
+        int answer = 0;
+        for (int i = 0; i < triangle[triangle.length - 1].length; i++) {
+            answer = Math.max(answer, triangle[triangle.length - 1][i]);
+        }
+
+        return answer;
+    }
+}
+
+/**
+ * 7
+ * 3 8
+ * 8 1 0
+ * 2 7 4 4
+ * 4 5 2 6 5
+ *
+ * 7
+ * 10 15
+ * 18 16 15
+ */
