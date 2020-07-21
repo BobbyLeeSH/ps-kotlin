974e9713721784f3fbd3ab408fe4b5c3f8703970
BobbyLeeSH <tkdgus1797@gmail.com>
2020-07-21 09:40:27 +0900

    no message

---
+++ src/kickstart2020/KickstartTemplate.kt
@@ -0,0 +1,11 @@
+package kickstart2020
+
+fun main() {
+    val cases = readLine()!!.toInt()
+    for (case in 1..cases) {
+        var count = 0
+        // TODO. write your code here
+
+        println("Case #$case: $count")
+    }
+}
