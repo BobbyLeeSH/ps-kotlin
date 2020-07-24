e347b8d1f329a0a9cf405fa85e93665e8caa9f1f
BobbyLeeSH <tkdgus1797@gmail.com>
2020-07-24 21:38:55 +0900

    no message

---
+++ src/kickstart2020/roundD/RecordBreaker.kt
@@ -0,0 +1,24 @@
+package kickstart2020.roundD
+
+fun main() {
+    val cases = readLine()!!.toInt()
+    for (case in 1..cases) {
+        val size = readLine()!!.toInt()
+        val visitors = readLine()!!.split(" ").map { it.toInt() }
+        var count = countRecordBreaker(visitors, size)
+
+        println("Case #$case: $count")
+    }
+}
+
+private fun countRecordBreaker(visitors: List<Int>, size: Int): Int {
+    var count = 0
+    var max = 0
+    for (i in 0 until size) {
+        val one = i == 0 || visitors[i] > max
+        val two = i == size - 1 || visitors[i] > visitors[i + 1]
+        if (one && two) count++
+        max = Math.max(visitors[i], max)
+    }
+    return count
+}
