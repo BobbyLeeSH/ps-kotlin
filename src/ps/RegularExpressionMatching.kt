9b6f3c98cad862b1f3be09d9c43cd35fa088bc1f
BobbyLeeSH <tkdgus1797@gmail.com>
2020-07-26 11:41:07 +0900

    no message

--- src/ps/RegularExpressionMatching.kt
+++
@@ -1,9 +0,0 @@
-package ps
-
-class RegularExpressionMatching {
-    fun isMatch(s: String, p: String): Boolean {
-        if (s.isEmpty() || p.isEmpty() || p[0] == '*') return false
-
-        return true
-    }
-}
