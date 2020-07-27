80b070ccfcc731c48e5a6d264a7cad91ddd396b2
BobbyLeeSH <tkdgus1797@gmail.com>
2020-07-27 13:46:33 +0900

    no message

--- src/leetcode/RegularExpressionMatching.kt
+++
@@ -1,9 +0,0 @@
-package leetcode
-
-class RegularExpressionMatching {
-    fun isMatch(s: String, p: String): Boolean {
-        if (s.isEmpty() || p.isEmpty() || p[0] == '*') return false
-
-        return true
-    }
-}
---
+++ src/leetcode/RemoveNthNode.kt
@@ -0,0 +1,37 @@
+package leetcode
+
+class RemoveNthNode {
+
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+
+        var nthNode = head
+        var curr = head
+
+        repeat(n) {
+            nthNode = nthNode?.next
+        }
+        if (nthNode == null) return head?.next
+
+        var temp = head?.next
+
+        while (true) {
+            nthNode = temp
+
+            repeat(n) {
+                nthNode = nthNode?.next
+            }
+            if (nthNode == null) break
+
+            temp = temp?.next
+            curr = curr?.next
+        }
+
+        curr?.next = temp?.next
+        return head
+    }
+}
+
+data class ListNode(
+        var `val`: Int,
+        var next: ListNode?
+)
